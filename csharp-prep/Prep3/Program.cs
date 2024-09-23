9aae1011f5b7a6508dd953f4bc860271d3708be1
carlnelson275 <zachnelson275@gmail.com>
2024-09-23 14:31:04 -0600

    prep 3 solution

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rnd = new Random();
+        int number = rnd.Next(1, 101);
+        string response;
+        int guess = -1;
+        while (guess != number)
+        {
+            Console.WriteLine("What's your guess?  ");
+            response = Console.ReadLine();
+            guess = int.Parse(response);
+
+            if (guess < number) 
+            {
+                Console.WriteLine("Too low :(");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Too high :(");
+            }
+            else if (guess == number)
+            {
+                Console.WriteLine("You got it!");
+            }
+        }
     }
 }
