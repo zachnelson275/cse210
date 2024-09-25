d2ff92a7f1ac6126c427813b8cd71473307fbee8
carlnelson275 <zachnelson275@gmail.com>
2024-09-25 14:40:18 -0600

    solved Prep4 program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,3 +1,31 @@
 using System;
+using System.Runtime.CompilerServices;
 
+List<int> numbers = new List<int>();
+int entry = -1;
+string intoInt;
+int average;
+int sum = 0;
+int length = 0;
 
+Console.WriteLine("Enter numbers for your list, enter 0 when finished");
+
+while (entry != 0)
+{
+    Console.Write("Enter a number:  ");
+    intoInt = Console.ReadLine();
+    entry = int.Parse(intoInt);
+    numbers.Add(entry);
+}
+
+foreach (int number in numbers)
+{
+    sum += number;
+}
+
+length = numbers.Count - 1;
+average = sum / length;
+
+Console.WriteLine();
+Console.WriteLine($"The sum is:     {sum}");
+Console.WriteLine($"The average is: {average}");
