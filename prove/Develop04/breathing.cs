6280391caed7249e3a155c145845a48b3d1941f5
carlnelson275 <zachnelson275@gmail.com>
2024-11-04 14:54:38 -0700

    started breathing

---
+++ prove/Develop04/breathing.cs
@@ -0,0 +1,23 @@
+class Breathing : Activity
+{
+    private int _breathCycle;
+    public void StartBreathing()
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome to the Breathing Activity!");
+        Console.WriteLine();
+        Console.WriteLine("This activity will help you relax by breathing in and out slowly.");
+        Console.WriteLine("Clear your mind, and get ready.");
+        base.LoadingWheel(6);
+    }
+    public int GetBreathCycle()
+    {
+        double time = base.SetTimer();
+        int cycles = (int)Math.Floor(time / 10);
+        return cycles;
+    }
+    public void Breathing()
+    {
+        
+    }
+}
