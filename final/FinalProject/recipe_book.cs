acb49d3aa083d5ab381d6a9994ec117f04c649be
carlnelson275 <zachnelson275@gmail.com>
2024-12-09 14:31:38 -0700

    recipebook completed

--- final/FinalProject/recipe_book.cs
+++ final/FinalProject/recipe_book.cs
@@ -1,29 +1,57 @@
 public class RecipeBook
 {
     private List<Food> _allFoods;
-    int _recipeCount;
+    private int _recipeCount;
+    public RecipeBook()
+    {
+        _allFoods = [];
+        _recipeCount = 0;
+    }
     public void DisplayAll()
     {
-
+        foreach (Food food in _allFoods)
+        {
+            food.DisplayRecipe();
+        }
     }
     public void DisplayBreakfast()
     {
-
+        foreach (Breakfast breakfast in _allFoods)
+        {
+            breakfast.DisplayRecipe();
+        }
     }
     public void DisplayLunch()
     {
-
+        foreach (Lunch lunch in _allFoods)
+        {
+            lunch.DisplayRecipe();
+        }
     }
     public void DisplayDinner()
     {
-
+        foreach (Dinner dinner in _allFoods)
+        {
+            dinner.DisplayRecipe();
+        }
     }
     public void DisplaySnack()
     {
-
+        foreach (Snack snack in _allFoods)
+        {
+            snack.DisplayRecipe();
+        }
     }
     public void DisplayDessert()
     {
-
+        foreach (Dessert dessert in _allFoods)
+        {
+            dessert.DisplayRecipe();
+        }
+    }
+    public int GetTotalRecipeCount()
+    {
+        _recipeCount = _allFoods.Count();
+        return _recipeCount;
     }
 }
